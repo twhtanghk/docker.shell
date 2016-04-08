2a879941e189a479a0ca09178789ae9ad81761f8
Tommy Tang <twhtanghk@gmail.com>
2016-04-08 11:43:04 +0800

    remove source volume

--- bin/oauth2.sh
+++ bin/oauth2.sh
@@ -6,6 +6,6 @@ dir="${root}/oauth2"
 
 image="twhtanghk/oauth2:latest"
 name="${1:+--name $1}"
-vol="${vol} -v ${root}/OAuth2:/usr/src/app -v ${dir}/conf/env.py:/usr/src/app/organization/env.py -v ${dir}/log:/usr/src/app/log"
+vol="${vol} -v ${dir}/conf/env.py:/usr/src/app/organization/env.py -v ${dir}/log:/usr/src/app/log"
 
 docker_run
