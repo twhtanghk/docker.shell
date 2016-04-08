036bf6a9a86073b04abbb5a26d11d9268f1877a5
Tommy Tang <twhtanghk@gmail.com>
2016-04-08 11:17:17 +0800

    update image

--- bin/oauth2.sh
+++ bin/oauth2.sh
@@ -4,7 +4,7 @@ file=$(readlink -f "$0")
 . $(dirname $file)/lib.sh
 dir="${root}/oauth2"
 
-image="python:2"
+image="twhtanghk/oauth2:latest"
 name="${1:+--name $1}"
 vol="${vol} -v ${root}/OAuth2:/usr/src/app -v ${dir}/conf/env.py:/usr/src/app/organization/env.py -v ${dir}/log:/usr/src/app/log"
 
