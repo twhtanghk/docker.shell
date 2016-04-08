0d4fdf7f20329f6502eb00f8391a7d54de8b23fb
Tommy Tang <twhtanghk@gmail.com>
2016-04-08 18:02:52 +0800

    add port 443

--- bin/docker.sh
+++ bin/docker.sh
@@ -8,7 +8,7 @@ image="docker:dind"
 name="${1:+--name $1}"
 env="${env} --privileged"
 dns="--dns=127.0.0.1"
-port="-p 2375:2375 -e PORT=2375 -p 80:80 -p 3306:3306 -p 53:53/udp -p 53:53/tcp"
+port="-p 2375:2375 -e PORT=2375 -p 80:80 -p 443:443 -p 53:53/udp"
 vol="${vol} -v ${dir}/image:/var/lib/docker -v ${root}:/root"
 
-docker_run 
+docker_run
