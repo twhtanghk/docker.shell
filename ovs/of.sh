1058be21446e5eb44a6fc4821631ff94eaa465c0
myroot <myroot@drum.mobile.ogcio.hksarg>
2016-04-29 13:18:40 +0800

    add ovs/of.sh and update compose file

---
+++ ovs/of.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+NAME=$1			# vlan name
+VIF=$2			# virtual interface
+LOCAL=$3		# local ip of virtual interface
+
+# see https://blog.remibergsma.com/2015/03/26/connecting-two-open-vswitches-to-create-a-l2-connection/
+# e.g.
+# ./of.sh internet vi0 localip
+# ./of.sh mgt vi1 localip
+# ./of.sh storage vi2 localip
+# ./of.sh intranet vi3 localip
+ovs-vsctl add-br ${NAME}
+ovs-vsctl add-port ${NAME} ${VIF} -- set Interface ${VIF} type=internal
+ifconfig ${VIF} ${LOCAL} netmask 255.255.255.0 up
