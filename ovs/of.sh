b7bfa6427393c43d0dc979e3e83c9be01ad33062
myroot <myroot@drum.mobile.ogcio.hksarg>
2016-04-29 17:17:23 +0800

    update floodlight compose file

--- ovs/of.sh
+++ ovs/of.sh
@@ -7,10 +7,10 @@ CTL=$4			# openflow controller (e.g. tcp:192.168.63.3:6653)
 
 # see https://blog.remibergsma.com/2015/03/26/connecting-two-open-vswitches-to-create-a-l2-connection/
 # e.g.
-# ./of.sh internet vi0 localip
-# ./of.sh mgt vi1 localip
-# ./of.sh storage vi2 localip
-# ./of.sh intranet vi3 localip
+# ./of.sh internet vi0 localip ctrlip
+# ./of.sh mgt vi1 localip ctrlip
+# ./of.sh storage vi2 localip ctrlip
+# ./of.sh intranet vi3 localip ctrlip
 ovs-vsctl add-br ${NAME}
 ovs-vsctl add-port ${NAME} ${VIF} -- set Interface ${VIF} type=internal
 ifconfig ${VIF} ${LOCAL} netmask 255.255.255.0 up
