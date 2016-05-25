b00c9e44e4b3d17b59f9dc3457733551caefb626
Tommy Tang <twhtanghk@gmail.com>
2016-05-25 10:01:16 +0800

    update mysql and oauth2 compose file

--- oauth2/conf/env.py
+++ oauth2/conf/env.py
@@ -26,7 +26,7 @@ DATABASES = {
         'NAME': 'org',  # Or path to database file if using sqlite3.
         'USER': 'root',  # Not used with sqlite3.
         'PASSWORD': '',  # Not used with sqlite3.
-        'HOST': 'mysql.mariadb.docker',  # Set to empty string for localhost. Not used with sqlite3.
+        'HOST': 'mysql',  # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',  # Set to empty string for default. Not used with sqlite3.
     }
 }
