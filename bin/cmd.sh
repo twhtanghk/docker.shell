70362b47a61864076d3265cff12c0b221a97f3b8
myroot <twhtanghk@gmail.com>
2017-03-28 12:51:31 +0800

    upgrade to docker-compose v3

--- bin/cmd.sh
+++ bin/cmd.sh
@@ -1,8 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
 cmd=`basename $0`
 proj=$1
 shift
 
-export COMPOSE_OPTIONS="-e COMPOSEROOT=${PWD} -e INTRANETIP=${INTRANETIP}"
+export COMPOSE_OPTIONS="-e COMPOSEROOT=${PWD}"
+for i in DISPLAY; do
+  COMPOSE_OPTIONS="${COMPOSE_OPTIONS} -e ${i}=${!i}"
+done
+  
 bin/docker-compose -f ${proj}/docker-compose.yml ${cmd} $@
