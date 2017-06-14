99a84622fab5d8f288e72824d37c7e6fd7c8c1be
myroot <twhtanghk@gmail.com>
2017-06-14 18:34:07 +0800

    add memory size

--- libvirt/data/start.sh
+++ libvirt/data/start.sh
@@ -1,1 +1,1 @@
-qemu-system-arm -M vexpress-a9 -kernel ./vmlinuz-3.2.0-4-vexpress -initrd ./initrd.gz -append root=/dev/mmcblk0p1 -sd root.img
+qemu-system-arm -m 1G -M vexpress-a9 -kernel ./vmlinuz-3.2.0-4-vexpress -initrd ./initrd.gz -append root=/dev/mmcblk0p1 -sd root.img
