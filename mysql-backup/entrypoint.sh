894a284bfd6b02ef7ef2806db77cf33a1ea2d9c1
Tommy Tang <twhtanghk@gmail.com>
2017-07-12 04:53:09 +0000

    create env.sh before source lib.sh

--- mysql-backup/entrypoint.sh
+++ mysql-backup/entrypoint.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-. lib.sh
+printenv |grep '^DB' |sed 's/^/export /g' >./env.sh
+
+. ./lib.sh
 
 mountEncfs ${ENCHOST}:/data/backup /mnt
 chown root.root /etc/logrotate.conf
 chown root.crontab /var/spool/cron/crontabs/root
-printenv |grep '^DB' |sed 's/^/export /g' >/root/env.sh
 cron -f -L 15
