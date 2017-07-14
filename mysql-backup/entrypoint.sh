5fc9e355b50a1718551234a7924d343ef7c490e9
Tommy Tang <twhtanghk@gmail.com>
2017-07-14 02:41:34 +0000

    update mount point

--- mysql-backup/entrypoint.sh
+++ mysql-backup/entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 printenv |grep '^DB' |sed 's/^/export /g' >./env.sh
-
-. ./lib.sh
-
-mountEncfs ${ENCHOST}:/data/backup /mnt
 chown root.root /etc/logrotate.conf
 chown root.crontab /var/spool/cron/crontabs/root
 chmod 600 /var/spool/cron/crontabs/root
+
+. ./lib.sh
+
+mountEncfs ${ENCHOST}:/data/backup/mysql /mnt && \
 cron -f
