b9d0daca69f760e4e5f4e1758bd5a98c33058bb2
Tommy Tang <twhtanghk@gmail.com>
2017-07-14 01:33:55 +0000

    chmod crontab config

--- mysql-backup/entrypoint.sh
+++ mysql-backup/entrypoint.sh
@@ -7,4 +7,5 @@ printenv |grep '^DB' |sed 's/^/export /g' >./env.sh
 mountEncfs ${ENCHOST}:/data/backup /mnt
 chown root.root /etc/logrotate.conf
 chown root.crontab /var/spool/cron/crontabs/root
+chmod 600 /var/spool/cron/crontabs/root
 cron -f
